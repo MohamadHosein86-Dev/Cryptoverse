1b0720d4631d06cb5f6b6f7632a3c3881594aa0a
MohamadHosein86-Dev <mohamadhoseinsafaryan86@gmail.com>
2025-06-12 17:31:48 +0330

    Edite-Ui

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="w-full h-14 border-t-[1px] border-[#1A1A1A] flex justify-center items-center gap-1">
-      Made by <Link to="https://www.linkedin.com/in/npouriaa" target="_blank" className="text-[#FF9332] underline">Pouria</Link>
+      Made by{" "}
+      <Link to="https://github.com/MohamadHosein86-Dev" target="_blank" className="text-[#FF9332] underline">
+        MohamadHosein
+      </Link>
     </footer>
   );
 };
