2f3e572305b4fe42d1d15eb11c36861fb5e9c140
MohamadHosein86-Dev <mohamadhoseinsafaryan86@gmail.com>
2025-06-12 17:01:45 +0330

    Fix/Bug

--- src/pages/CoinDetails.tsx
+++ src/pages/CoinDetails.tsx
@@ -13,19 +13,15 @@ import SelectDays from "../components/coinDetails/SelectDays";
 import { SelectChangeEvent } from "@mui/material";
 import TogglePriceType from "../components/coinDetails/TogglePriceType";
 import Description from "../components/coinDetails/Description";
-
+//
 const CoinDetails = () => {
   const { coinId } = useParams();
-  const [coin, setCoin] = useState<CoinDetailsObjectType | undefined>(
-    undefined
-  );
+  const [coin, setCoin] = useState<CoinDetailsObjectType | undefined>(undefined);
   const [error, setError] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(true);
   const [priceType, setPriceType] = useState<string>("prices");
   const [days, setDays] = useState<number | string>(30);
-  const [chartData, setChartData] = useState<ChartDataType>(
-    {} as ChartDataType
-  );
+  const [chartData, setChartData] = useState<ChartDataType>({} as ChartDataType);
 
   const getData = async () => {
     setLoading(true);
@@ -44,18 +40,11 @@ const CoinDetails = () => {
     setLoading(false);
   };
 
-  const handleDaysChange = async (
-    event: SelectChangeEvent<number | string>
-  ) => {
+  const handleDaysChange = async (event: SelectChangeEvent<number | string>) => {
     if (coinId) {
       setLoading(true);
       setDays(event.target.value);
-      const prices = await getPrices(
-        coinId,
-        event.target.value,
-        priceType,
-        setError
-      );
+      const prices = await getPrices(coinId, event.target.value, priceType, setError);
       if (prices) {
         settingChartData(setChartData, prices);
         setLoading(false);
@@ -63,10 +52,7 @@ const CoinDetails = () => {
     }
   };
 
-  const handlePriceTypeChange = async (
-    _event: MouseEvent<HTMLElement>,
-    newPriceType: string
-  ) => {
+  const handlePriceTypeChange = async (_event: MouseEvent<HTMLElement>, newPriceType: string) => {
     if (coinId && newPriceType) {
       setPriceType(newPriceType);
       console.log(newPriceType);
@@ -87,24 +73,16 @@ const CoinDetails = () => {
         <div className="flex flex-col justify-start max-sm:p-4 md2:p-0 items-center gap-6 max-sm:w-full md2:w-[95%] md3:w-[90%] xl:w-[90%] 2xl:w-[62.5rem]">
           <Details coin={coin} />
           <div className=" bg-[#0D0D0D] rounded-xl p-4 w-full">
-            <SelectDays
-              days={days}
-              handleDaysChange={handleDaysChange}
-              pTag={false}
-            />
-            <TogglePriceType
-              priceType={priceType}
-              handlePriceTypeChange={handlePriceTypeChange}
-            />
+            <SelectDays days={days} handleDaysChange={handleDaysChange} pTag={false} />
+            <TogglePriceType priceType={priceType} handlePriceTypeChange={handlePriceTypeChange} />
             <DataChart chartData={chartData} />
           </div>
-            <Description description={coin.description} name={coin.name}/>
+          <Description description={coin.description} name={coin.name} />
         </div>
       ) : error ? (
         <div className="p-4 2xl:w-1/3 sm2:w-3/5 max-sm:w-full">
           <div className="p-4 rounded-xl bg-[#0D0D0D] text-red-500 text-center text-sm">
-            {error} - Error while loading {coinId} details. Please try again
-            later
+            {error} - Error while loading {coinId} details. Please try again later
           </div>
         </div>
       ) : (
